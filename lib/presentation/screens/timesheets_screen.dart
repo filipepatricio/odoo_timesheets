895b6f81d29aa7c68d62f2378880d4ff011f6cb7
Filipe Patricio <facpatricio@gmail.com>
2024-04-23 11:42:08 +0200

    Add empty list container

--- lib/presentation/screens/timesheets_screen.dart
+++ lib/presentation/screens/timesheets_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:odoo_apexive/blocs/bloc_exports.dart';
 import 'package:odoo_apexive/presentation/screens/create_timer_screen.dart';
 import 'package:odoo_apexive/presentation/styles/app_dimens.dart';
+import 'package:odoo_apexive/presentation/styles/vector_graphics.dart';
 import 'package:odoo_apexive/presentation/widgets/app_bar/app_bar_big_title.dart';
 import 'package:odoo_apexive/presentation/widgets/gradient_scaffold.dart';
 import 'package:odoo_apexive/presentation/widgets/app_bar/main_app_bar.dart';
+import 'package:odoo_apexive/presentation/widgets/opacity_button.dart';
 import 'package:odoo_apexive/presentation/widgets/timer_card.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -21,17 +24,61 @@ class TimesheetsScreen extends StatelessWidget {
       ),
       body: BlocBuilder<TaskListBloc, TaskListState>(
         builder: (context, state) {
-          return ListView.separated(
-            itemCount: state.taskTimerList.length,
-            itemBuilder: (context, index) => TimerCard(
-              taskTimer: state.taskTimerList[index],
-            ),
-            separatorBuilder: (context, index) => const SizedBox(
-              height: AppDimens.s,
-            ),
-          );
+          return state.taskTimerList.isNotEmpty
+              ? ListView.separated(
+                  itemCount: state.taskTimerList.length,
+                  itemBuilder: (context, index) => TimerCard(
+                    taskTimer: state.taskTimerList[index],
+                  ),
+                  separatorBuilder: (context, index) => const SizedBox(
+                    height: AppDimens.s,
+                  ),
+                )
+              : const _EmptyList();
         },
       ),
+    );
+  }
+}
+
+class _EmptyList extends StatelessWidget {
+  const _EmptyList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                AppVectorGraphics.emptyTimesheets,
+              ),
+              const SizedBox(height: AppDimens.xl),
+              Text(
+                AppLocalizations.of(context)!.empty_timesheets_title,
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium!
+                    .copyWith(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: AppDimens.s),
+              Text(
+                AppLocalizations.of(context)!.empty_timesheets_call_to_action,
+                style: Theme.of(context).textTheme.bodyLarge,
+              )
+            ],
+          ),
+        ),
+        OpacityButton(
+          title: AppLocalizations.of(context)!.get_started,
+          onTap: () =>
+              Navigator.pushNamed(context, CreateTimerScreen.routeName),
+        ),
+      ],
     );
   }
 }
--- lib/presentation/styles/vector_graphics.dart
+++ lib/presentation/styles/vector_graphics.dart
@@ -9,4 +9,6 @@ class AppVectorGraphics {
       'assets/vector_graphics/suitcase_border_outlined.svg';
   static const String starFilled = 'assets/vector_graphics/star_filled.svg';
   static const String chevronDown = 'assets/vector_graphics/chevron_down.svg';
+  static const String emptyTimesheets =
+      'assets/vector_graphics/empty_timesheets.svg';
 }
